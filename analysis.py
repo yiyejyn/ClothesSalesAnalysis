4daf9371dc523156d7b03cf887e5e420ba8845db
5OH-HO5 <omj0414@daum.net>
2019-12-18 14:30:41 +0900

    final analysis

--- analysis.py
+++ analysis.py
@@ -2,6 +2,13 @@ from flask import Flask, escape, request, render_template
 import random
 from bs4 import BeautifulSoup
 import requests
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+import pandas as pd
+import numpy as np
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,12 +22,37 @@ def analysis():
 
 @app.route('/result')
 def result():
-    col1 = request.args.get('col1')
-    col2 = request.args.get('col2')
-    col3 = request.args.get('col3')
-    col4 = request.args.get('col4')
-    col5 = request.args.get('col5')
-    return render_template('result.html', col1=col1, col2=col2, col3=col3, col4=col4, col5=col5)
+    POPULARITY = int(request.args.get('POPULARITY'))
+    BUCKET = int(request.args.get('BUCKET'))
+    TOTAL_SELL = int(request.args.get('TOTAL_SELL'))
+    LIKE_ = int(request.args.get('LIKE_'))
+    REVIEW = int(request.args.get('REVIEW'))
+    PRICE = int(request.args.get('PRICE'))
+    df_train = pd.read_csv("df_train.csv")
+
+    y_label = df_train['FIRST_PAGE']
+    df_train = df_train.drop(['Unnamed: 0','FIRST_PAGE'] , axis = 1)
+    df_train.to_csv("test")
+
+    X_train , X_test , y_train , y_test =train_test_split(df_train , y_label, random_state = 156) 
+
+    dt_clf = DecisionTreeClassifier()
+
+    dt_clf = dt_clf.fit(X_train,y_train)
+    dt_prediction = dt_clf.predict(X_test)
+
+    confusion_matrix(y_test , dt_prediction, labels=[1,0])
+
+    accuracy = accuracy_score(y_test , dt_prediction)
+    print("정확도",accuracy)
+
+    new_data01 = np.array([[POPULARITY , BUCKET, TOTAL_SELL, LIKE_,REVIEW,PRICE]])
+    plus_ = dt_clf.predict(new_data01)
+    if plus_[0] == 1:
+        plus = "상위 페이지에 등록되기에 충분합니다!!"
+    else:
+        plus = "상위 페이지에 등록되기엔 부족합니다."
+    return render_template('result.html', POPULARITY=POPULARITY, BUCKET=BUCKET, TOTAL_SELL=TOTAL_SELL, LIKE_=LIKE_, REVIEW=REVIEW, PRICE=PRICE, plus=plus)
 
 if __name__ == '__main__':
     app.run(debug=True)
